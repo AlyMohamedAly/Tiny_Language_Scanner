e0515d0a4715bb1f323fce9085b89f660a833de8
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2018-04-18 01:24:38 +0200

    Changed the name of the file and added file reading :D

---
+++ TinyScanner.py
@@ -0,0 +1,45 @@
+ff = open("put your path here")
+s = ff.readline()
+KeyWords = ["if", "then", "else", "end", "repeat", "until", "read", "write"]
+Symbols = ['+', '-', '*', '/', '<', '>', '=', ':=', '(', ')']
+while len(s) != 0:		#Optimize this
+    s = s.rstrip()
+    ind, temp2 =0, 0
+    while ind in range(len(s)):
+        if s[ind] == '{':
+            temp2 = ind
+            ind = s[ind:].find('}')+ind
+            print s[temp2:ind+1], ": Comment"
+            temp2 = ind + 1
+        else:
+            if s[ind] == ' ' or s[ind] == ';' or ind == len(s)-1:
+                if len(s[temp2:ind]) > 0 or temp2 == len(s) - 1:
+                    if s[temp2:ind] in KeyWords or s[temp2:] in KeyWords:
+                        if ind == len(s)-1 and s[ind] != ';':
+                            print s[temp2:], ": Reserved Word"
+                        else:
+                            print s[temp2:ind], ": Reserved Word"
+                        temp2 = ind + 1
+                    elif s[temp2:ind] in Symbols or s[temp2:] in Symbols:
+                        if ind == len(s)-1 and s[ind] != ';':
+                            print s[temp2:], ": Symbol"
+                        else:
+                            print s[temp2:ind], ": Symbol"
+                        temp2 = ind + 1
+                    elif '0' <= s[temp2] <= '9':
+                        if ind == len(s)-1 and s[ind] != ';':
+                            print s[temp2:], ": Digit"
+                        else:
+                            print s[temp2:ind], ": Digit"
+                        temp2 = ind + 1
+                    else:
+                        if ind == len(s)-1 and s[ind] != ';':
+                            print s[temp2:], ": Identifier"
+                        else:
+                            print s[temp2:ind], ": Identifier"
+                        temp2 = ind + 1
+                else:
+                    temp2 = ind +1
+        ind += 1
+    s = ff.readline()
+ff.close()
