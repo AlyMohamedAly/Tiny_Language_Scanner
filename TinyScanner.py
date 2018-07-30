5b0f6f60a09857365a4718cbbf3a1aeae6ce4f35
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2018-07-30 19:49:38 +0200

    Big Update
    The Scanner now works properly and even detects variable name errors

--- TinyScanner.py
+++ TinyScanner.py
@@ -1,80 +1,61 @@
-ff = open("your path here")
-s = ff.readline()
+def NumOrVar(A):
+    if 'a' <= A[0] <= 'z' or 'A' <= A[0] <= 'Z':
+        return ProperVariable(A)
+    elif '0' <= A[0] <= '9':
+        return ProperInt(A)
+    else:
+        return "NaN"
+
+
+def ProperVariable(A):
+    for k in A:
+        if not ('a' <= k <= 'z' or 'A' <= k <= 'Z' or '0' <= k <= '9'):
+            return "NaN"
+    return "Variable"
+
+
+def ProperInt(A):
+    for k in A:
+        if not ('0' <= k <= '9'):
+            return "NaN"
+    return "Number"
+
+
+def SplitSymb(A):
+    temp = A
+    for k in Symbols:
+        if k in A:
+            temp = (" " + k + " ").join(temp.split(k))
+    return temp
+
 KeyWords = ["if", "then", "else", "end", "repeat", "until", "read", "write"]
-Symbols = ['+', '-', '*', '/', '<', '>', '=', ':=', '(', ')', ';']
-while len(s) != 0:		#Optimize this
-    s = s.strip('\n').strip('\t').strip(' ')
-    ind, temp2 = 0, 0
-    while ind in range(len(s)):
-        if s[ind] == ' ' or ind == len(s)-1 or s[ind] == '{':
-            if len(s[temp2:ind]) > 0 or temp2 == len(s) - 1:
-                if s[temp2:ind] in KeyWords or s[temp2:] in KeyWords:
-                    if ind == len(s)-1:
-                        if s[ind] != ';':
-                            print s[temp2:], ": Reserved Word"
-                        else:
-                            print s[temp2:ind], ": Reserved Word"
-                            print s[ind], ": Symbol"
-                    else:
-                        print s[temp2:ind], ": Reserved Word"
-                    temp2 = ind + 1
-                elif s[temp2:ind] in Symbols or s[temp2:] in Symbols:
-                    if ind == len(s)-1:
-                        print s[temp2:], ": Symbol"
-                    else:
-                        print s[temp2:ind], ": Symbol"
-                    temp2 = ind + 1
-                elif '0' <= s[temp2] <= '9':
-                    Flag = False
-                    for i in s[temp2:ind]:
-                        if 'a' <= i <= 'z' or 'A' <= i <= 'Z':
-                            Flag = True
-                    if ind == len(s)-1:
-                        if s[ind] != ';':
-                            if Flag:
-                                print s[temp2:], ": Digit error"
-                            else:
-                                print s[temp2:], ": Digit"
-                        else:
-                            if Flag:
-                                print s[temp2:ind], ": Digit error"
-                            else:
-                                print s[temp2:ind], ": Digit"
-                            print s[ind], ": Symbol"
-                    else:
-                        print s[temp2:ind], ": Digit"
-                    temp2 = ind + 1
-                elif 'a' <= s[temp2] <= 'z' or 'A' <= i <= 'Z':
-                    Flag = False
-                    for i in s[temp2:ind]:
-                        if '0' <= i <= '9':
-                            Flag = True
-                    if ind == len(s)-1:
-                        if s[ind] != ';':
-                            if Flag:
-                                print s[temp2:], ": Identifier error"
-                            else:
-                                print s[temp2:], ": Identifier"
-                        else:
-                            if Flag:
-                                print s[temp2:ind], ": Identifier error"
-                            else:
-                                print s[temp2:ind], ": Identifier"
-                            print s[ind], ": Symbol"
-                    else:
-                        print s[temp2:ind], ": Identifier"
-                    temp2 = ind + 1
-                else:
-                    print s[temp2:ind], ": Unknown Symbol"
-                    temp2 = ind + 1
-            else:
-                temp2 = ind + 1
-            if s[ind] == '{':
-                temp2 = ind
-                ind = s[ind:].find('}') + ind
-                print s[temp2:ind + 1], ": Comment"
-                temp2 = ind + 1
-        ind += 1
-    print ""
-    s = ff.readline()
+Symbols = ['+', '-', '*', '/', '<', '>', ':=', '(', ')', ';', '{', '}']
+print ""
+ff = open("F:\Input.txt", 'r')
+text = ff.readlines()
 ff.close()
+
+for s in text:
+    s = ((s.strip('\n')).strip('\t')).strip(' ')
+    s = (SplitSymb(s).strip(' ')).split()
+    i, sz = 0, len(s)
+    while i < sz:
+        if s[i] in KeyWords:
+            print s[i] + " : KeyWord"
+        elif s[i] in Symbols:
+            if s[i] == '{':
+                temp = s.index('}')
+                print ' '.join(s[i+1:temp]) + " : Comment"
+                i = temp+1
+            else:
+                print s[i] + " : Symbol"
+        else:
+            x = NumOrVar(s[i])
+            if x == "Nan":
+                print s[i] + " : Syntax Error"
+            elif x == "Variable":
+                print s[i] + " : Variable"
+            elif x == "Number":
+                print s[i] + " : Number"
+        i += 1
+    print ""
